0f1ca7eb245aa2f276abe85041ff99c149baa165
alaakhaireldin <alaa.khaireldin@gmail.com>
2023-01-08 18:25:10 +0100

    Using inline styles

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,7 +4,14 @@ import "./modal.css";
 export default function Modal({ children, handleClose }) {
   return createPortal(
     <div className="modal-backdrop">
-      <div className="modal">
+      <div
+        className="modal"
+        style={{
+          border: "4px solid",
+          borderColor: "#ff4500",
+          textAlign: "center",
+        }}
+      >
         {children}
         <br></br>
         <button onClick={handleClose}>close</button>
