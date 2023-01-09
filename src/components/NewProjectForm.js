b1304f026d15373108c04f95caee85bd18883924
alaakhaireldin <alaa.khaireldin@gmail.com>
2023-01-09 21:42:49 +0100

    Using onChange event to get access to written data in the form

--- src/components/NewProjectForm.js
+++ src/components/NewProjectForm.js
@@ -1,24 +1,57 @@
+import { useState } from "react";
 import "./NewProjectForm.css";
 
 export default function NewProjectForm() {
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [data, setData] = useState("");
+  const [unit, setUnit] = useState("");
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
   return (
     <form className="new-project-form">
       <label>
         <span> Project Title: </span>
-        <input type="text"></input>
+        <input
+          type="text"
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        ></input>
       </label>
       <label>
         <span> Project Image: </span>
-        <input type="img"></input>
+        <input
+          type="file"
+          onChange={(e) => {
+            setImage(e.target.value);
+          }}
+        ></input>
       </label>
       <label>
         <span> Project Unit: </span>
-        <input type="text"></input>
+        <input
+          type="text"
+          onChange={(e) => {
+            setUnit(e.target.value);
+          }}
+        ></input>
       </label>
       <label>
         <span> Project Data: </span>
-        <input type="text"></input>
+        <input
+          type="text"
+          onChange={(e) => {
+            setData(e.target.value);
+          }}
+        ></input>
       </label>
+      <p>
+        title - {title}, Image - {image}
+      </p>
       <button>Submit</button>
     </form>
   );
